fa139f2d4f20ffc39fe440e657f6fd652173448b
Lev Rednikov <120374210+CheateRYT@users.noreply.github.com>
2024-04-09 09:52:54 +0300

    action set_username

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,12 +1,26 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setUsername } from "./reducers/userReducer"; // Обновленный импорт
 
 const MyComponent = () => {
   const username = useSelector((state) => state.user.username);
+  const dispatch = useDispatch();
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    dispatch(setUsername(inputValue));
+    setInputValue("");
+  };
 
   return (
     <div>
       <h1>Hello, {username}!</h1>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleButtonClick}>Change Username</button>
     </div>
   );
 };
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,12 +1,20 @@
 const initialState = {
   username: "Max",
 };
-
+const SET_USERNAME = "SET_USERNAME";
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_USERNAME:
+      return {
+        ...state,
+        username: action.payload,
+      };
     default:
       return state;
   }
 };
-
+export const setUsername = (username) => ({
+  type: SET_USERNAME,
+  payload: username,
+});
 export default userReducer;
